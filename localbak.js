78bc968e40ce31cd431f665c6e00d6ff17d5e050
Matthew Evans <mevans+git@wtfsystems.net>
2022-06-09 22:31:14 -0400

    wip

--- localbak.js
+++ localbak.js
@@ -17,14 +17,18 @@ const constants = {
     BACKUP_FOLDER: `_backup`
 }
 
-const processFolder = (location, backup_location) => {
+const processFolder = (location, backup_location, ignoreList) => {
+    if(ignoreList === undefined) ignoreList = []
     const fileList = fs.readdirSync(location, { withFileTypes: "true" })
     fileList.forEach(item => {
+        //  Skip the backup folder
         if(`${location}/${item.name}` == `${process.cwd()}/${constants.BACKUP_FOLDER}`) return
         //  Check for ignore
+        if(ignoreList.find(ignore => { item.name == ignore})) return
+        //  Process the item
         if(item.isDirectory()) {
             fs.mkdirSync(`${backup_location}/${item.name}`)
-            processFolder(`${location}/${item.name}`, `${backup_location}/${item.name}`)
+            processFolder(`${location}/${item.name}`, `${backup_location}/${item.name}`, ignoreList)
         } else fs.copyFileSync(`${location}/${item.name}`, `${backup_location}/${item.name}`)
     })
 }
@@ -41,6 +45,9 @@ if(!settings) settings = {}
 //  Overrwite backup name if exists in settings
 if(settings['backup_name']) constants.BACKUP_FOLDER = settings['backup_name']
 
+//  If an extra name is specified, add it to the backup folder name
+if(process.argv[2] != undefined) constants.BACKUP_FOLDER += process.argv[2]
+
 //  Remove old backup
 fs.rmSync(`${process.cwd()}/${constants.BACKUP_FOLDER}`, {recursive: true, force: true})
 
@@ -50,6 +57,6 @@ try {
 } catch (err) { wtf.scriptError(err) }
 
 //  Process the backup
-processFolder(process.cwd(), `${process.cwd()}/${constants.BACKUP_FOLDER}`)
+processFolder(process.cwd(), `${process.cwd()}/${constants.BACKUP_FOLDER}`, settings['ignore'])
 
 process.stdout.write(`\n${wtf.colors.GREEN}Done!${wtf.colors.CLEAR}\n`)
